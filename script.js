0d27cbe20c5e5ff5927bfe275b3ecb7fc3a4614f
Ilia <my_ilia@mail.ru>
2022-12-17 21:01:51 +0300

    Новый файл js

---
+++ script.js
@@ -0,0 +1,10 @@
+//for (var i = 1 ; i < 21 ; i++) { console.log('q', i) }
+var c = 10;
+let d = 15;
+function sum(a,b){
+    let result = a + b + c + d;
+    return result;
+}
+
+var result = sum(2, 3);
+console.log(result);
